4d48baccb9143c3680655df1b6cbf4f3a8aa38d0
mersocarlin <mersocarlin@gmail.com>
2015-04-24 22:27:18 -0300

    added es6 support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 /* jshint node: true */
 var path = require('path');
+var webpack = require('webpack');
 
 
 module.exports = {
@@ -10,25 +11,44 @@ module.exports = {
     path: path.join(__dirname),
     filename: 'mersocarlin-template.js',
     libraryTarget: 'umd',
-    library: 'MersocarlinTemplate'
+    library: 'MersoCarlinTemplate'
   },
 
   externals: {
    'react': 'var React'
   },
 
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
   module: {
     loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       {
         test: /\.scss$/,
-        // Query parameters are passed to node-sass
-        loader: 'style!css!sass?outputStyle=expanded&' +
-          'includePaths[]=' + (path.resolve(__dirname, './bower_components')) + '&' +
-          'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
+        loader: 'style!css!sass?outputStyle=expanded&'
+        + 'includePaths[]=' + (path.resolve(__dirname, './bower_components'))
+        + '&'
+        + 'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
       },
       {
-        test: /\.jsx$/,
-        loader: 'jsx-loader?harmony'
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'imports?_=lodash&$=jquery!jsx?harmony!babel-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   }
