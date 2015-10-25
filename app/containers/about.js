7e4d4ba2755ba8dfbc2a48efd8a464c5545ddf25
mersocarlin <mersocarlin@gmail.com>
2015-10-25 18:20:28 -0200

    indenting code

--- app/containers/about.js
+++ app/containers/about.js
@@ -3,23 +3,23 @@ import React from 'react';
 
 export default React.createClass({
 
-    render() {
-      const { query } = this.props.location;
-      const params = this.props.params;
+  render() {
+    const { query } = this.props.location;
+    const params = this.props.params;
 
-      return (
-        <div className="app-page page-about">
-          About Page
+    return (
+      <div className="app-page page-about">
+        About Page <br/>
 
-          <div>
-            Query: {JSON.stringify(query)}
-          </div>
-
-          <div>
-            Params: {JSON.stringify(params)}
-          </div>
+        <div>
+          Query: {JSON.stringify(query)}
         </div>
-      );
-    }
 
-  });
+        <div>
+          Params: {JSON.stringify(params)}
+        </div>
+      </div>
+    );
+  }
+
+});
