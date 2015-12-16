d8867d259040ce0eb240ede3edf9fc15415b299e
mersocarlin <mersocarlin@gmail.com>
2015-12-16 20:53:50 -0200

    Add react rules to eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     ],
     "rules": {
       "react/prop-types": 0,
+      "react/jsx-boolean-value": 0,
+      "consistent-return": 0,
       "space-before-function-paren": [2, { "anonymous": "never", "named": "always" }]
     }
 };
