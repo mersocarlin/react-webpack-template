33911a4e2c902514b49ce290d5362bdd5f1d87dc
mersocarlin <mersocarlin@gmail.com>
2016-01-16 16:32:37 -0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-      "react/prop-types": 0,
-      "react/jsx-boolean-value": 0,
-      "consistent-return": 0,
-      "space-before-function-paren": [2, { "anonymous": "never", "named": "always" }]
-    }
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+    "react/prop-types": 0,
+    "react/jsx-boolean-value": 0,
+    "consistent-return": 0,
+    "no-use-before-define": 0,
+    "space-before-function-paren": [2, { "anonymous": "never", "named": "always" }]
+  }
 };
