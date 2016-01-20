90cea980a415ce35c54d50e7364ac481fc680844
mersocarlin <mersocarlin@gmail.com>
2016-01-20 19:08:52 -0200

    Disable guard-for-in eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "react/prop-types": 0,
     "react/jsx-boolean-value": 0,
     "consistent-return": 0,
+    "guard-for-in": 0,
     "no-use-before-define": 0,
     "space-before-function-paren": [2, { "anonymous": "never", "named": "always" }]
   }
